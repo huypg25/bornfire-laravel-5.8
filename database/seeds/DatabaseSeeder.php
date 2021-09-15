d54aa7c2f394a13a0c59ac39e0a4f3b4519130dd
Your Name <you@example.com>
2021-09-15 15:45:33 +0700

    categories section

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(AdminsTableSeeder::class);
         $this->call(SettingsTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
 
     }
 }
